23dd0b281136e5eb0c208ae10c903b1e60d3f3c4
VitaliiRomanenko <vitalii.o.romanenko@gmail.com>
2026-06-05 15:57:14 +0300

    feat: update settings structure and add TMDb API Key and template file options, rename plugin

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,18 +1,20 @@
 import { App, PluginSettingTab, Setting } from 'obsidian';
-import MyPlugin from '../main';
+import PopcornMD from '../main';
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface PopcornMDSettings {
+	APIKey: string;
+	templateFile: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default',
+export const DEFAULT_SETTINGS: PopcornMDSettings = {
+	APIKey: '',
+	templateFile: '',
 };
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class PopcornMDSettingTab extends PluginSettingTab {
+	plugin: PopcornMD;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PopcornMD) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -22,17 +24,43 @@ export class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
+		// 
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc("It's a secret")
+			.setName('TMDb API Key')
+			.setDesc("Your TMDb API Key. You can get one for free at https://www.themoviedb.org/documentation/api")
 			.addText((text) =>
 				text
-					.setPlaceholder('Enter your secret here')
-					.setValue(this.plugin.settings.mySetting)
+					.setPlaceholder('Enter your key')
+					.setValue(this.plugin.settings.APIKey)
 					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
+						this.plugin.settings.APIKey = value;
 						await this.plugin.saveSettings();
 					}),
 			);
+		
+		new Setting(containerEl)
+			.setName('Template')
+			.setDesc(this.plugin.settings.templateFile == "" 
+							? "The template used to generate the movie note" 
+							: `Current template: ${this.plugin.settings.templateFile}`
+						)
+			.addButton((button) => {
+				button.setButtonText("Chose a template file")
+				.onClick(() => {
+					const input = document.createElement('input');
+					input.type = 'file';
+					input.accept = '.md';
+					input.onchange = async () => {
+						const file = input.files?.[0];
+						if (file) {
+							this.plugin.settings.templateFile = file.name;
+							await this.plugin.saveSettings();
+							this.display();
+						}
+					};
+					input.click();
+				});
+			});
+			
 	}
 }
