f728683083b22582c445b1bf8e3e9a09f5505052
VitaliiRomanenko <vitalii.o.romanenko@gmail.com>
2026-06-07 21:14:35 +0300

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
-  globals: {
-    "ts-jest": {
-      tsconfig: 'tsconfig.jest.json'
-    },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }]
   },
   moduleFileExtensions: ["ts", "js", "json"],
   testMatch: ["**/tests/**/*.test.ts"],
