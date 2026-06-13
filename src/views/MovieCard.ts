0a8801cde038c449792332071d776948b7a0aa1a
VitaliiRomanenko <vitalii.o.romanenko@gmail.com>
2026-06-13 19:07:49 +0300

    refactor: improve createMovieCard function by replacing innerHTML with DOM manipulation for better performance and readability

--- src/views/MovieCard.ts
+++ src/views/MovieCard.ts
@@ -1,3 +1,4 @@
+import { info } from "console";
 import { SearchMovie } from "../models/SearchResult";
 
 export function createMovieCard(movie: SearchMovie, genresMap: Record<number, string>): HTMLElement {
@@ -5,31 +6,67 @@ export function createMovieCard(movie: SearchMovie, genresMap: Record<number, st
   card.className = "card";
   card.id = movie.id.toString();
 
-  let genresHtml = "";
-  if (movie.genre_ids && movie.genre_ids.length > 0) {
-    genresHtml = `
-      <div class="genres">
-        ${movie.genre_ids.map(id => `
-          <div class="genre"><span>${genresMap[id] ?? ""}</span></div>
-        `).join("")}
-      </div>
-    `;
-  }
+  //#region Photo
+  const photoDiv = document.createElement("div");
+  photoDiv.className = 'movie-photo';
+  const img = document.createElement("img");
+  img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  img.alt = movie.title;
+  photoDiv.appendChild(img);
+  //#endregion
 
-  card.innerHTML = `
-    <div class="movie-photo">
-      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}"/>
-    </div>
-    <div class="movie-info">
-      <h2>${movie.title}</h2><span class="original-name">(${movie.original_title})</span>
-      <p>${movie.overview}</p>
-      ${genresHtml}
-      <div class="movie-subinfo">
-        <span class="realise-year">${movie.release_date.length === 0 ? "" : movie.release_date.slice(0, 4)}</span>
-    </div>
-  `;
+  //#region Info
+  const infoDiv = document.createElement("div");
+  infoDiv.className = "movie-info";
+  
+  const titleEl = document.createElement("h2");
+  titleEl.textContent = movie.title;
+  infoDiv.appendChild(titleEl);
+
+  const originalTitleEl = document.createElement("span");
+  originalTitleEl.textContent = `(${movie.original_title})`;
+  originalTitleEl.className = "original-name";
+  infoDiv.appendChild(originalTitleEl);
+
+  const overviewEl = document.createElement("p");
+  overviewEl.textContent = movie.overview;
+  infoDiv.appendChild(overviewEl);
+  //#endregion
+
+  //#region Genres
+  if(movie.genre_ids?.length){
+    const genresDiv = document.createElement("div");
+    genresDiv.className = "genres";
+    movie.genre_ids.forEach(id => {
+      const genre = document.createElement("div");
+      genre.className = "genre";
+      const span = document.createElement("span");
+      span.textContent = genresMap[id] ?? "";
+      genre.appendChild(span);
+      genresDiv.appendChild(genre);
+    });
+    infoDiv.appendChild(genresDiv);
+  };
+
+  //#endregion
+
+  //#region Subinfo
+  const subInfoDiv = document.createElement("div");
+  subInfoDiv.className = "movie-subinfo";
+  
+  const yearSpan = document.createElement("span");
+  yearSpan.className = "realise-year";
+  yearSpan.textContent = movie.release_date ? movie.release_date.slice(0, 4) : "";
+  subInfoDiv.appendChild(yearSpan);
+
   const ratingEl = renderStars(movie.vote_average);
-  card.querySelector(".movie-subinfo")?.appendChild(ratingEl);
+  subInfoDiv.appendChild(ratingEl);
+  infoDiv.appendChild(subInfoDiv);
+
+  //#endregion
+
+  card.appendChild(photoDiv);
+  card.appendChild(infoDiv);
 
   function renderStars(rating: number): HTMLDivElement {
     const container = document.createElement("div");
